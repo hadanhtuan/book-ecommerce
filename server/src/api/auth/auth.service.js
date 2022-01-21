21cef5ca62ed9d905316a3c9fdac9674e73a8df6
iL3sor <92763293+iL3sor@users.noreply.github.com>
2022-01-21 17:35:43 +0700

    Update auth.service.js

--- server/src/api/auth/auth.service.js
+++ server/src/api/auth/auth.service.js
@@ -3,87 +3,79 @@ var jwt = require("jsonwebtoken");
 var User = require("../../models/user");
 
 async function register(body) {
-  async function register(body) {
     const email = body.email;
     const password = body.password;
-
+    
     try {
-      const user = await User.findOne({ email: email });
-      if (user) {
+        const user = await User.findOne({ email: email })
+        if (user) {
+            return {
+                error: true,
+                message: "Email đã được sử dụng",
+            };
+        }    
+        const newUser = new User({
+            email,
+            password
+        });
+            
+        let salt = await bcrypt.genSalt(10)
+        let hash = await bcrypt.hash(newUser.password, salt)
+        
+        if (hash.err) 
+            throw err;
+                    
+        newUser.password = hash;
+        
+        await newUser.save()
+                        
+        const _token = jwt.sign({ _id: newUser._id }, "secret");
         return {
-          error: true,
-          message: "Email đã được sử dụng",
-        };
-      }
-      const newUser = new User({
-        email,
-        password,
-      });
-
-      let salt = await bcrypt.genSalt(10);
-      let hash = await bcrypt.hash(newUser.password, salt);
-
-      if (hash.err) throw err;
-
-      newUser.password = hash;
-
-      await newUser.save();
-
-      const _token = jwt.sign({ _id: newUser._id }, "secret");
-      return {
-        error: false,
-        message: "Đăng ký thành công!",
-        token: _token,
-      };
-    } catch (err) {
-      return {
-        error: true,
-        message: err.message,
-      };
+            error: false,
+            message: "Đăng ký thành công!",
+            token: _token
+        };     
     }
-  }
+    catch(err) {
+        return {
+            error: true,
+            message: err.message
+        }
+    }
 }
-
 async function login(body) {
-  try {
-    const user = await User.findOne({ email: body.email });
-    if (!user) {
-      return {
-        error: true,
-        message: "Email hoặc mật khẩu không đúng",
-      };
-    }
-    const result = await bcrypt.compare(body.password, user.password);
-
-    if (!result) {
-      console.log(result.res);
-      return {
-        error: true,
-        message: "Email hoặc mật khẩu không đúng",
-      };
-    }
-    const accessToken = jwt.sign(
-      {
-        _id: user._id
-      },
-      "secret"
-    );
-
-    if (result) {
-      return {
-        error: false,
-        message: "Đăng nhập thành công",
-        token: accessToken,
-      };
-    }
-  } catch (err) {
-    return {
+    const email = body.email;
+    const password = body.password;
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+        return {
+            error: true,
+            message: "Tài khoản hoặc mật khẩu không đúng"
+        };
+        } else {
+            const passcmp = await bcrypt.compare(password, user.password);
+            if (passcmp) {
+                var _token = jwt.sign({ _id: user._id }, "secret");
+                return {
+                    error: false,
+                    message: "Đăng nhập thành công",
+                    token: _token,
+                };
+            } else {
+                return {
+                error: true,
+                message: "Tài khoản hoặc mật khẩu không đúng"
+                };
+            }
+        }
+    } catch (errorr) {
+        return {
         error: true,
         message: "Đăng nhập thất bại"
+        };
     }
-  }
 }
-
 async function forgotPassword() {}
 async function resetPassword() {}
 
