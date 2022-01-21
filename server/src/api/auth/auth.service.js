b0cf8cd8b034280438f58d0664c987d58288dc7f
iL3sor <92763293+iL3sor@users.noreply.github.com>
2022-01-21 15:36:14 +0700

    Update auth.service.js

--- server/src/api/auth/auth.service.js
+++ server/src/api/auth/auth.service.js
@@ -4,45 +4,46 @@ const User = require("../../models/user");
 
 
 async function register(body) {
-  const email = body.email;
-  const password = body.password;
-    await User.findOne({ email: email }).then((user) => {
-      if (user) {
-        return {
-          error: true,
-          message: "Email đã được sử dụng",
-        };
-      } else {
+    const email = body.email;
+    const password = body.password;
+    
+    try {
+        const user = await User.findOne({ email: email })
+        if (user) {
+            return {
+                error: true,
+                message: "Email đã được sử dụng",
+            };
+        }    
         const newUser = new User({
-          email,
-          password
+            email,
+            password
         });
-         bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => {
-                var _token = await jwt.sign({ _id: user._id }, "secret");
-                return {
-                  error: false,
-                  message: "Đăng ký thành công!",
-                  token: _token
-                };
-              })
-              .catch((err) => {
-                console.log(err);
-                return {
-                  error: true,
-                  message: "Đã xảy ra lỗi khi đăng ký",
-                };
-              });
-          });
-        });
-      }
-    });
-  }
+            
+        let salt = await bcrypt.genSalt(10)
+        let hash = await bcrypt.hash(newUser.password, salt)
+        
+        if (hash.err) 
+            throw err;
+                    
+        newUser.password = hash;
+        
+        await newUser.save()
+                        
+        const _token = jwt.sign({ _id: newUser._id }, "secret");
+        return {
+            error: false,
+            message: "Đăng ký thành công!",
+            token: _token
+        };     
+    }
+    catch(err) {
+        return {
+            error: true,
+            message: err.message
+        }
+    }
+}
 async function login(body) {
   const email = body.email;
   const password = body.password;
