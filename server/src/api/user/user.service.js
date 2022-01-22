f7def7ea36b82d7df09699e27ae936f688efc6e2
nghihua <huanhatgianghi@gmail.com>
2022-01-22 15:56:55 +0700

    done get-orders feature

---
+++ server/src/api/user/user.service.js
@@ -0,0 +1,37 @@
+var Orders = require("../../models/orders");
+var User = require("../../models/user");
+var jwt = require("jsonwebtoken");
+
+async function getOrders(token){
+    try {
+        let decodedToken = await jwt.verify(token, "secret");
+
+        let userId = decodedToken._id;
+
+        let orders = await Orders.find({ userId });
+
+        if(!orders){
+            return {
+                error: true,
+                message: 'Không có dữ liệu'
+            }
+        }
+
+        return {
+            error: false,
+            message: "Lấy danh sách đã mua thành công",
+            orders
+        }
+
+    }
+    catch {
+        return {
+            error: true,
+            message: err.message,
+        }
+    }
+}
+
+module.exports= {
+    getOrders
+}
