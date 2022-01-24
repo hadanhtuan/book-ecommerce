8cc2c5d5b8fe6de4a40aa874d38d98f9f416a37c
TrongTin <trantrongtin01012002@gmail.com>
2022-01-24 14:34:58 +0700

    register update

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -9,8 +9,11 @@ import { register } from "./auth/authAction";
 const RegisterForm = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-
-  const {isLoading, message, error } = useSelector(state => state.auth)
+  const [passwordLength, setPasswordLength] = useState(false);
+  const [containsNumbers, setContainsNumbers] = useState(false);
+  const [isUpperCase, setIsUpperCase] = useState(false);
+  const [isEqualPassword, setIsEqualPassword] = useState(false);
+  const { isLoading, message, error } = useSelector((state) => state.auth);
 
   const [require, setRequire] = useState({
     confirmPw: "",
@@ -21,15 +24,62 @@ const RegisterForm = () => {
     password: "",
   });
 
-  const handleFormChange = (e) => {
+  // check Number
+  const checkForNumbers = (string) => {
+    var matches = string.match(/\d+/g);
+
+    setContainsNumbers(matches != null ? true : false);
+  };
+
+  // check for upper case
+  const checkForUpperCase = (string) => {
+    var matches = string.match(/[A-Z]/);
+
+    setIsUpperCase(matches != null ? true : false);
+  };
+
+  const handleEmailChange = (e) => {
+    setUserInfo({ ...userInfo, email: e.target.value });
+  };
+
+  const handlePasswordChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault()
-
-    dispatch(register(userInfo, history));
+  const handleConfirmPasswordChange = (e) => {
+    setRequire({ ...require, confirmPw: e.target.value });
   };
+
+  const handleFormSubmit = async (e) => {
+    if (passwordLength && containsNumbers && isUpperCase && isEqualPassword) {
+      e.preventDefault();
+      console.log("submit", userInfo);
+      console.log("cf", require.confirmPw);
+
+      dispatch(register(userInfo, history));
+    } else {
+      e.preventDefault();
+
+      alert("Vui lòng nhập mật khẩu ít nhất có 8 kí tự và chứa 1 chữ in Hoa");
+    }
+  };
+  useEffect(() => {
+    console.log(" ");
+    checkForNumbers(userInfo.password);
+    checkForUpperCase(userInfo.password);
+    setPasswordLength(userInfo.password.length > 7 ? true : false);
+    if (require.confirmPw && userInfo.password === require.confirmPw) {
+      setIsEqualPassword(true);
+    } else {
+      setIsEqualPassword(false);
+    }
+    console.log("confirm", isEqualPassword);
+    console.log("confirmpw", require.confirmPw);
+    console.log("password", userInfo.password);
+    console.log("contain number", containsNumbers);
+    console.log("upperCase", isUpperCase);
+  }, [userInfo.password, require.confirmPw]);
+
   return (
     <div className="col-lg-6">
       <div className="card2 card border-0 px-4 pb-4 mb-4">
@@ -39,17 +89,20 @@ const RegisterForm = () => {
       <div className="row px-3 mb- mt-4">
         <h2 className="mb-0 mr-4 mt-2">Create Account</h2>
       </div>
-        {message && (
-                    <div class={`alert alert-${error ? 'danger' : 'success'}`} role="alert">
-                        {message}
-                    </div>
-                )}
-                
-        {isLoading && (
-                 <div class="spinner-border" role="status">
-                    <span class="sr-only">Loading...</span>
-                </div>
-        )}
+      {message && (
+        <div
+          className={`alert alert-${error ? "danger" : "success"}`}
+          role="alert"
+        >
+          {message}
+        </div>
+      )}
+
+      {isLoading && (
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
       <form onSubmit={handleFormSubmit}>
         <div className="row px-3">
           <label className="mb-1">
@@ -60,7 +113,7 @@ const RegisterForm = () => {
             type="text"
             name="email"
             placeholder="Enter a valid email "
-            onChange={(e) => handleFormChange(e)}
+            onChange={(e) => handleEmailChange(e)}
             value={userInfo.email}
             required
           />
@@ -74,9 +127,9 @@ const RegisterForm = () => {
             type="password"
             name="password"
             placeholder="Enter password"
-            required
-            onChange={(e) => handleFormChange(e)}
+            onChange={(e) => handlePasswordChange(e)}
             value={userInfo.password}
+            required
           />
         </div>
         <div className="row px-3">
@@ -85,21 +138,16 @@ const RegisterForm = () => {
           </label>
           <input
             className="mb-4"
-            type="confirmpassword"
+            type="password"
             name="confirmpassword"
             placeholder="Enter to confirm password"
+            onChange={(e) => handleConfirmPasswordChange(e)}
             value={require.confirmPw}
-            onChange={(e) =>
-              setRequire({ ...require, confirmPw: e.target.value })
-            }
             required
           />
         </div>
         <div className="row mb-3 px-3">
-          <button
-            type="submit"
-            className="btn btn-blue text-center"
-          >
+          <button type="submit" className="btn btn-blue text-center">
             Create
           </button>
         </div>
@@ -116,7 +164,12 @@ const RegisterForm = () => {
             required
           />
           <label for="my-input" className="custom-control-label">
-            <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">I accept the Terms of Use & Privacy Policy</Link>
+            <Link
+              to="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+            >
+              I accept the Terms of Use & Privacy Policy
+            </Link>
           </label>
         </div>
       </div>
