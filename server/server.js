f8c8c9f313cb8bc58902f5131631e903c9882a9d
hadanhtuan <20522109@gm.uit.edu.vn>
2022-01-24 22:30:59 +0700

    404

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const ErrorResponse = require("./utils/errorResponse");
 
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-app.use(express.json())
+app.use(express.json({  limit: '30mb', extended: true }));
+app.use(express.urlencoded({  limit: '30mb', extended: true }));
 
 app.use(express.static('public'))
 
@@ -21,15 +21,17 @@ const api = require('./src/api')
 //routing
 app.use('/api/', api)
 
+//404 not found
+app.use((req, res) => {
+    console.log(__dirname);
+    res.status(404).sendFile(path.join(__dirname, '/public/404.html'))
+})
 
 //error handle
 app.use((err, req, res, next) => {
     let error = { ...err };
   
     error.message = err.message;
-  
- 
-    console.log(error.message);
   
     res.status(error.statusCode || 500).json({
       error: true,
@@ -43,5 +45,3 @@ mongoose.connect('mongodb://localhost/book-ecommerce',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
-
